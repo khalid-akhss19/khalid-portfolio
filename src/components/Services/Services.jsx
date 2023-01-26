536a1b97c8f6cc435e986b9e988cda2f72c6f986
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2023-01-26 22:27:59 +0500

    first commit

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './Khalid Hussain - CV.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <spane>
+        I have been a freelance developer for nearly two years, during which 
+        time I have had the opportunity <br /> to work with agencies, consult for 
+         startups, and collaborate with other talented individuals on web projects <br />
+          for both business and consumer use. My primary area of expertise is front-end development, <br /> including HTML, CSS, JS, React JS, and Material UI. I am   
+          skilled in creating  responsive <br /> websites that are fast, user-friendly, and 
+           built using best practices.<br /> In addition, I have experience with   popular open-source CMS platforms <br /> such as WordPress, Webflow, and Wix. For more 
+            information, <br />  please visit my <a href="">LinkedIn</a>  profile or contact me directly.
+          {/* Lorem ispum is simpley dummy text of printing of printing Lorem
+          <br />
+          ispum is simpley dummy text of printing */}
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, React, Nodejs "}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Wordpress"}
+            detail={
+              "Lorem ispum dummy text are usually use in section where we need some random text"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
